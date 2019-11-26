496bf4cbbc2e76c308894cb5342c6b1371f27988
Ahrkano <36827623+Ahrkano@users.noreply.github.com>
2019-11-25 22:25:57 -0300

    fase 0, fase 1, fase 2, intro added

---
+++ scripts/intro.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var img00 = load("res://assets/environment/misc/sekai.png")
+var img01 = load("res://assets/environment/misc/death.png")
+var img02 = load("res://assets/environment/misc/stevan.png")
+var img03 = load("res://assets/environment/misc/despair.png")
+var img04 = load("res://assets/environment/misc/vengeance.png")
+
+var images = [img00,img01,img02,img03,img04]
+var img_id = 1
+
+onready var picture = $TextureRect
+
+signal finished
+
+func _ready():
+	picture.texture = images[0]
+
+func change_img(id):
+	if img_id >= 5:
+		emit_signal("finished")
+	else:
+		picture.texture = images[id]
+		img_id += 1
+
+func _on_DialogueUI_next_img():
+	change_img(img_id)
+
+
+func _on_Node_finished():
+	get_tree().change_scene("res://scenes/levels/fase_01.tscn")
